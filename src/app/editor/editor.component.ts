78d0a14a0678f6d845379019e181872e781dde4f
Harald Schwab <hs@rigbit.at>
2021-12-01 10:33:52 +0100

    add editor to container

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as CodeMirror from 'codemirror';
 import { EditorFromTextArea } from 'codemirror';
 
@@ -7,7 +7,7 @@ import { EditorFromTextArea } from 'codemirror';
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements AfterViewInit {
+export class EditorComponent implements AfterViewInit, OnDestroy {
   @ViewChild('code', { static: true }) public code!: ElementRef;
   @ViewChild('editor', { static: true }) public editor!: ElementRef;
 
@@ -19,20 +19,35 @@ export class EditorComponent implements AfterViewInit {
     'dracula',
   ];
 
+  private resizeObserver: ResizeObserver;
+
+  constructor(private elRef: ElementRef) {
+    this.resizeObserver = new ResizeObserver(_ => {
+      this.codeMirrorEditor?.refresh();
+    });
+  }
+
   ngAfterViewInit(): void {
     if (!this.code) { return; }
+    this.resizeObserver.observe(this.elRef.nativeElement.parentElement);
+
     const options = {
       lineNumbers: true,
       mode: 'application/javascript',
       styleActiveLine: true,
       matchBrackets: true,
       autoCloseBrackets: true,
+      theme: 'dracula',
       tabSize: 2
     }
 
     this.codeMirrorEditor = CodeMirror.fromTextArea(this.code.nativeElement, options);
     console.log(this.codeMirrorEditor);
     this.codeMirrorEditor.refresh();
+  }
+
+  ngOnDestroy(): void {
+    this.resizeObserver.disconnect();
   }
 
   public updateTheme(): void {
