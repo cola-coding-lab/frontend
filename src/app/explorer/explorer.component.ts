e93912b08115c257d1eb420f90803e3d8a1a361a
Harald Schwab <hs@rigbit.at>
2021-12-14 15:32:31 +0100

    remove unneeded code

--- src/app/explorer/explorer.component.ts
+++ src/app/explorer/explorer.component.ts
@@ -7,8 +7,6 @@ import { EditorFile, FileService } from '../file/file.service';
   styleUrls: ['./explorer.component.scss'],
 })
 export class ExplorerComponent implements OnInit {
-  // @ViewChild('newFile') newFile?: ElementRef;
-
   public root: EditorFile = {name: '/', type: 'directory', children: []}
   public addFile = false;
 
@@ -33,56 +31,4 @@ export class ExplorerComponent implements OnInit {
     console.log('select', file);
     this.fileService.select(file);
   }
-
-  /*
-   public add($event: KeyboardEvent): void {
-   switch ($event.key) {
-   case 'Enter':
-   this.saveAddFile();
-   break;
-   case 'Escape':
-   this.hideAddFile();
-   break;
-   }
-   }
-
-
-   public showAddFile(): void {
-   this.addFile = true;
-   timer(50).subscribe(_ => {
-   if (this.newFile?.nativeElement) {
-   this.newFile.nativeElement.focus();
-   }
-   });
-   }
-   public hideAddFile(): void {
-   if (this.newFile?.nativeElement) {
-   this.newFile.nativeElement.value = undefined;
-   }
-   this.addFile = false;
-   }
-   public saveAddFile(): void {
-   if (this.newFile?.nativeElement) {
-   let fileName = this.newFile.nativeElement.value;
-   if (fileName === '') {
-   alert('pleaser enter filename');
-   return;
-   }
-   let file: EditorFile;
-   if (/\.(js|css|html?|txt)$/i.test(fileName)) {
-   file = { name: fileName, type: getFileType(fileName, 'text/javascript'), content: '' };
-   } else {
-   fileName += `.js`;
-   file = { name: fileName, type: 'text/javascript', content: '' };
-   }
-   if (this.files.find(f => f.name === fileName)) {
-   alert('file already exists');
-   return;
-   }
-   this.fileService.select(file)
-   this.addFile = false;
-   this.select(file);
-   }
-   }
-   */
 }
