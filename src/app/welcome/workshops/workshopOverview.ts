486fe1aa11c1d8f4e6b5afdb93bc1c72b6c10f11
Harald Schwab <hs@rigbit.at>
2022-04-08 09:56:03 +0200

    fix: build error

--- src/app/welcome/workshops/workshopOverview.ts
+++ src/app/welcome/workshops/workshopOverview.ts
@@ -1,13 +1,10 @@
-import { Url } from "url";
-import {v4 as uuid} from "uuid";
-
 export interface WorkshopOverview {
   // TODO: how to import a uuid correctly (document it) and how to use it in an interface
-    id: string = uuid();
-    title: string;
-    description: string;
-    image: Url;
-    lessonsCount: number;
-    difficulty: string;
-    categories: string[];
-  }
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  lessonsCount: number;
+  difficulty: string;
+  categories: string[];
+}
