0c2ee2d86a928125561f57dbb0189919969c397f
Harald Schwab <hs@rigbit.at>
2022-05-09 20:47:54 +0200

    update header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ComputedComponent } from 'src/util/computed.component';
 import { setDocumentProperty } from 'src/util/properties';
+import { environment } from '../../environments/environment';
+import { CurrentProjectService } from '../project/current-project.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: [ './header.component.scss' ],
 })
 export class HeaderComponent extends ComputedComponent implements OnInit {
-  public title = 'Resizable';
+  public title = environment.title;
 
-  constructor(
-    // private draftService: DraftService
-  ) {
-    super();
+  constructor(private currentProjectService: CurrentProjectService) { super(); }
+
+  public get hasProject(): boolean {
+    return this.currentProjectService.activeProject !== undefined;
   }
 
   ngOnInit(): void {
